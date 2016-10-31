ca33cf97730eb8752aeb913f0e8c230c94ea7964
Syed Haris <syedharis49@gmail.com>
2016-11-01 00:18:50 +0500

    added Pattern

---
+++ Pattern/app.js
@@ -0,0 +1,40 @@
+function diamond( n ) 
+{
+    n = parseInt(n,10);
+
+    var i, s;
+
+    // top: 1 to n
+    document.write("<pre>");
+    for(i = 1; i <= n; i++ )
+    {
+        // write n-i spaces:
+        for ( s = 1; s <= n-i; s++ )
+        {
+            document.write(" ");
+        }
+        // then write i asterisk+space sets:
+        for ( s = 1; s <= i; s++ )
+        {
+            document.write(" *");
+        }
+        document.write("\n");
+    }
+    // bottom: n-1 down to 1
+    for(i = n-1; i >= 1; i-- )
+    {
+        // write n-i spaces:
+        for ( s = 1; s <= n-i; s++ )
+        {
+            document.write(" ");
+        }
+        // then write i asterisk+space sets:
+        for ( s = 1; s <= i; s++ )
+        {
+            document.write(" *");
+        }
+        document.write("\n");
+    }
+}
+
+diamond(20);
